96a5dfc8cfbcd9f2e7fa9fd1b915dc8b798cd763
MadEwink <adrien.floriant@telecom-sudparis.eu>
2020-01-05 21:01:12 +0100

    Deep Link
    When opening a specific URI in a browser, you can choose to open it
    	through the app, it takes you to a filled form if the id
    	corresponds to the id of one of your tasks
    	URI :https://android-tasks-api.herokuapp.com/tasks/{id}

--- app/src/main/java/com/example/td2/TaskFormActivity.kt
+++ app/src/main/java/com/example/td2/TaskFormActivity.kt
@@ -3,12 +3,16 @@ package com.example.td2
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProviders
+import com.example.td2.network.Api
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_taskform.*
 import kotlinx.android.synthetic.main.item_task.*
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 
 class TaskFormActivity : AppCompatActivity() {
 
@@ -16,12 +20,28 @@ class TaskFormActivity : AppCompatActivity() {
         ViewModelProviders.of(this).get(TaskViewModel::class.java)
     }
 
+    private val coroutineScope = MainScope()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_taskform)
-        form_title.setText(intent.getStringExtra("title")?:"")
-        form_description.setText(intent.getStringExtra("description")?:"")
-        val id = intent.getStringExtra("id")
+
+        var id : String? = intent?.data.toString()
+        if (id != "null")
+        {
+            id = id?.substringAfterLast('/')
+            coroutineScope.launch {
+                val tasks = Api.INSTANCE.tasksService.getTasks().body()
+                val task = tasks?.find { task : Task -> task.id == id}
+                form_title.setText(task?.title)
+                form_description.setText(task?.description)
+            }
+        }
+        else {
+            id = intent.getStringExtra("id")
+            form_title.setText(intent.getStringExtra("title") ?: "")
+            form_description.setText(intent.getStringExtra("description") ?: "")
+        }
         if (id == null)
             confirm_form.setOnClickListener {createTask(); backToMain()}
         else
